5d0b25999c5deb82ac315900d11f9565c0214b14
SoftwareIISGubbio <software@iisgubbio.gov.it>
2019-03-26 15:00:46 +0100

    sistemata grafica della pagina di svolgimento del questionario

--- WebContent/questionario_svolgi_form.php
+++ WebContent/questionario_svolgi_form.php
@@ -5,80 +5,71 @@ session_start();
 <html lang="it">
 <head>
     <meta charset="UTF-8">
-    <title> Questionario </title>  
+    <title>Questionario</title>  
+    <link rel="stylesheet" type="text/css" href="stili.css">
 </head>
 <body> 
-      <!-- apro un form con metodo post che possa permettere di passare i dati tra pagine 
-           riga di codice in javscript che ritorna ad una pagine php 
-           stampa un alert in caso non sia settato l’id-->
 <form action="questionario_svolgi_azione.php" method="post">
-     <?php
+    <?php
     if(!isset($_GET['id'])){
+        /* stampa un alert e ritorna all'indice in caso non sia settato l’id */
         echo '<script language="javascript">';
         echo 'alert("Id non è stato inserito");';
         echo 'window.location.href = "index.php";';
         echo '</script>';
-    }
-        else {
-            
+    } else {
         //stabilisco la connessione con il server
         $ID=$_GET['id'];
         $_SESSION['ID'] = $ID;
-        
-require "lib/connessione.php";
+        require "lib/connessione.php";
 
-
-//comando sql per la selezione dei dati all’interno della tabella questionario 
-//con l’id uguale a quello inserito
-$sql="SELECT * FROM questionari WHERE ID = $ID";
+        $sql="SELECT * FROM questionari WHERE ID = $ID";
   
-$result = mysqli_query($conn, $sql);
- //stampo il nome de questionario se c’è un risultato se no stampo un alert 
-//reindirizzo ad un'altra pagina
-if ($result && mysqli_num_rows($result) > 0) {
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "<H1 align=center style='color: chocolate'>${row['Nome']}</H1>";
-    }
-}
- else {
-        echo '<script language="javascript">';
-        echo 'alert("Id non è stato inserito");';
-        echo 'window.location.href = "index.php";';
-        echo '</script>';
-   }
+        $result = mysqli_query($conn, $sql);
+        // stampo il nome de questionario se c’è un risultato se no 
+        // stampo un alert e reindirizzo ad un'altra pagina
+        if ($result && mysqli_num_rows($result) > 0) {
+            while($row = mysqli_fetch_assoc($result)) {
+                echo "<H1 align=center style='color: chocolate'>${row['Nome']}</H1>";
+            }
+        } else {
+            echo '<script language="javascript">';
+            echo 'alert("Id non è stato inserito");';
+            echo 'window.location.href = "index.php";';
+            echo '</script>';
+        }
     
-//comando sql per la selezione delle domande che abbiano i seguesnti requisiti 
-$sqlDomande="SELECT domande.* FROM domande,domande_questionari,questionari 
-WHERE domande.ID_domanda=domande_questionari.FK_domanda and domande_questionari.FK_questionario=questionari.ID 
-and questionari.id='$ID'";
+        $sqlDomande="SELECT domande.* FROM domande,domande_questionari,questionari 
+            WHERE domande.ID_domanda=domande_questionari.FK_domanda and domande_questionari.FK_questionario=questionari.ID 
+            and questionari.id='$ID'";
 
-$resultDomande = mysqli_query($conn, $sqlDomande);
-//stampo il testo della domanda se sono state trovate righe e procedo con la stampa delle risposte
-if (mysqli_num_rows($resultDomande) > 0) {
-    while($row = mysqli_fetch_assoc($resultDomande)) {
-        echo"<h2>${row['Testo_domanda']}</h2>\n";
-//comando sql per la selezione delle risposte e del loro id 
-//corrispondenti a quella domanda 
-        $sqlRisposte="SELECT Testo_risposta, ID_risposta FROM risposte WHERE risposte.FK_domanda=${row['ID_domanda']}";
-        $id=$row['ID_domanda'];
-        $resultRisposte = mysqli_query($conn, $sqlRisposte);
-//stampo il testo della risposta vicino ad un radiobutton che serve per la sua selezione
-        if (mysqli_num_rows($resultRisposte) > 0) {
-            while($row = mysqli_fetch_assoc($resultRisposte)) {
-                   echo "<p><input type='radio' name=$id value=${row['ID_risposta']}>${row['Testo_risposta']}"; echo "</p>\n";
+        $resultDomande = mysqli_query($conn, $sqlDomande);
+        //stampo il testo della domanda se sono state trovate righe e procedo con la stampa delle risposte
+        if (mysqli_num_rows($resultDomande) > 0) {
+            while($row = mysqli_fetch_assoc($resultDomande)) {
+                echo "<div class='domanda'>\n";
+                echo"<h2>${row['Testo_domanda']}</h2>\n";
+                //comando sql per la selezione delle risposte e del loro id corrispondenti a quella domanda 
+                $sqlRisposte="SELECT Testo_risposta, ID_risposta FROM risposte WHERE risposte.FK_domanda=${row['ID_domanda']}";
+                $id=$row['ID_domanda'];
+                $resultRisposte = mysqli_query($conn, $sqlRisposte);
+                //stampo il testo della risposta vicino ad un radiobutton che serve per la sua selezione
+                if (mysqli_num_rows($resultRisposte) > 0) {
+                    while($row = mysqli_fetch_assoc($resultRisposte)) {
+                        echo "<p><input type='radio' name=$id value=${row['ID_risposta']}><label>${row['Testo_risposta']}"; echo "</label></p>\n";
+                    }
+                } else {
+                    echo "0 results";
+                }
+                echo "</div>\n";
             }
         } else {
             echo "0 results";
         }
+        
+        mysqli_close($conn);
+        echo '<p><button><span> Inivia questionario </span></button></p>';
     }
-} else {
-    echo "0 results";
-}
-    //chiudo la connessiione
-mysqli_close($conn);
-//creo un bottone per inviare i dati alla pagina successiva che si limiterà a verificare i risultati 
-     echo '<p><button><span> Inivia questionario </span></button></p>';
-        }
     ?> 
 </form>
 </body>
