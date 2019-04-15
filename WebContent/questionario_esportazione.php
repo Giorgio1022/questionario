7f223e0cfaa5f392d99310ce0af4ae8b1f7186e0
KryFioru <38553500+KryFioru@users.noreply.github.com>
2019-04-15 08:31:58 +0200

    Create questionario_esportazione.php

---
+++ WebContent/questionario_esportazione.php
@@ -0,0 +1,24 @@
+<?xml  version="1.0" encoding="UTF-8" ?>
+<question type="multichoice">
+     <name>
+         <domanda>Testo della domanda:</domanda>
+         <materia>XYZ</materia>
+     </name>
+     <questiontext format="html">
+     <text>Risposta1</text>
+     <shuffleanswers>0/1</shuffleanswers>
+     </questiontext>
+     <questiontext format="html">
+     <text>Risposta2</text>
+     <shuffleanswers>0/1</shuffleanswers>
+     </questiontext>
+     <questiontext format="html">
+     <text>Risposta3</text>
+     <shuffleanswers>0/1</shuffleanswers>
+     </questiontext>
+     <questiontext format="html">
+     <text>Risposta4</text>
+     <shuffleanswers>0/1</shuffleanswers>
+     </questiontext>
+     </question>
+   
